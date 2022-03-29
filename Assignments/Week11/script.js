bfa3f8be8fb534dd9765938dddc65a3e24727c81
jagreer7 <jagr4452@colorado.edu>
2022-03-29 10:04:32 -0600

    Creates script.js

--- Assignments/Week11/script.js
+++ Assignments/Week11/script.js
@@ -1,1 +1,8 @@
-const para = document.querySelector('query')
+const para = document.querySelector('p'); //Selecting all things in the document with the 'p' tag (paragraphs) and set them to the para constant
+
+para.addEventListener('click', updateName); //Wait for the user to click any paragraph (para) and execute updateName
+
+function updateName(){ //Funtion named update name
+  const name = promt ('Enter a New Name'); //Promt the user to enter a name
+  para.textContent = `Player 1: ${name}`; //Change the text content to player 1 then the name they enter
+}
