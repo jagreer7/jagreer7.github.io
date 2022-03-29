fcfbc97f134504129a73364b41c08ed732fab703
jagreer7 <jagr4452@colorado.edu>
2022-03-29 10:11:18 -0600

    Created js definitions sheet

---
+++ Assignments/Week11/jsDefinitions.js
@@ -0,0 +1,16 @@
+let myVariable;
+myVariable = 'Jackson';
+
+let myVarible2 = 'Joe';
+
+myVariable = 'Jim';
+
+/*
+Multiline Comment
+*/
+
+//Singleline Comment
+
+let myVarible3 = 'bob'; //String
+let myNumber = 3; //Number
+let myBoolean = true; //Boolean logic
