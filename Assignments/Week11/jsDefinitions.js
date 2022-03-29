c3807e017a4888c8ad4e9280a7de8be4ea991f38
jagreer7 <jagr4452@colorado.edu>
2022-03-29 10:22:33 -0600

    Updating Definitions sheet

--- Assignments/Week11/jsDefinitions.js
+++ Assignments/Week11/jsDefinitions.js
@@ -14,3 +14,31 @@ Multiline Comment
 let myVarible3 = 'bob'; //String
 let myNumber = 3; //Number
 let myBoolean = true; //Boolean logic
+let myArray = [1, 'bob', true]; //Array (which can hold multiple different data types)
+let myObject = document.querySelector('p'); //Stores any p (paragraph) object into my object
+
+//OPERATORS
+6 + 9; // ---> 15
+'Hello ' + 'World'; // ---> Hello World
+
+/*
+Addition -> x + y
+Subtraction -> x - y
+Multiplication -> x * y
+Division -> x / y
+
+Assignment -> x = y
+Equality -> x === y
+
+Does not equal -> !(x === y)
+Does not equal -> x !== y
+*/
+//CONDITIONALS
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate'){
+  alert('Yay, I love chocolate ice cream!');
+}
+else{
+  alert('Oh no, Not chocolate!')
+}
