53feaf0b522e0f7bd00e0b452717cdb3a7d2e1f8
jagreer7 <jagr4452@colorado.edu>
2022-04-05 10:45:25 -0600

    Cat Fact Generator is funcitoning

--- Assignments/Week12/week12-demo.js
+++ Assignments/Week12/week12-demo.js
@@ -2,9 +2,27 @@
 //document.querySelector('.new-quote button');
 
 const quoteButton = document.querySelector('#js-new-quote');
+const endPoint = 'https://catfact.ninja/fact';
 
-function getQuote(){
+async function  getQuote(){
   console.log("Quote button clicked");
+  try {
+    const response = await fetch(endPoint);
+    if(!response.ok){ //Throw an error if the API does not work (account, verification. etc...)
+      throw Error(response.statusText);
+    }
+    const json = await response.json(); //Getting the json that comes with the response
+    console.log(json.fact);
+    displayFact(json.fact);
+  } catch (err){
+    console.log(err);
+    alert('Failed');
+  }
+}
+
+function displayFact(fact){
+  const factText = document.querySelector('#js-quote-text');
+  factText.textContent = fact;
 }
 
 quoteButton.addEventListener('click', getQuote);
