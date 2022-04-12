9416cfa7c781919abe3ef55c855995f4d200d875
jagreer7 <jagr4452@colorado.edu>
2022-04-12 10:22:05 -0600

    Random Color generator

--- Assignments/Week13/week13-demo.js
+++ Assignments/Week13/week13-demo.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const WIDTH = document.documentElement.clientWidth;
-const HEIGHT = document.documentElement.clientHeight;
+const HEIGHT =  document.documentElement.clientHeight;
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -14,12 +14,40 @@ function random(number) {
 
 function draw() {
   ctx.clearRect(0,0,WIDTH,HEIGHT);
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     ctx.beginPath();
-    ctx.fillStyle = 'rgba(255,0,0,0.5)';
+    ctx.fillStyle = "rgba(" + random(255) + ", " + random(255) + ", " + random(255) + ", 0.5)";
     ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
     ctx.fill();
   }
 }
+let abort = 0;
+function resetAbort(){
+  abort = 0;
+  mycode();
+}
 
-btn.addEventListener('click',draw);
+//var tid = setTimeout(mycode, 100);
+
+function mycode() {
+  if(abort != 100){
+    ctx.clearRect(0,0,WIDTH,HEIGHT);
+    for (let i = 0; i < 1000; i++) {
+      ctx.beginPath();
+      ctx.fillStyle = "rgba(" + random(255) + ", " + random(255) + ", " + random(255) + ", 0.5)";
+      ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+      ctx.fill();
+    }
+    abort = abort + 1;
+    tid = setTimeout(mycode, 100); // repeat myself
+  }
+  else{
+    abortTimer();
+  }
+
+}
+function abortTimer() { // to be called when you want to stop the timer
+  clearTimeout(tid);
+}
+
+btn.addEventListener('click',resetAbort);
