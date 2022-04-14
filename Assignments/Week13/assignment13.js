9af47ac9c89a8c314c45fdb5a98c7bf2799d245b
jagreer7 <jagr4452@colorado.edu>
2022-04-14 15:28:06 -0600

    Update assignment13.js

--- Assignments/Week13/assignment13.js
+++ Assignments/Week13/assignment13.js
@@ -12,16 +12,16 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-var newStory = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-var name = "Bob";
+var newStory = storyText;
+var nameVariable = "Bob";
 
 function result() {
   if(customName.value !== '') {
-    name = customName.value;
-    console.log(name);
+    nameVariable = customName.value;
+    console.log(nameVariable);
   }
   else{
-    name = "Bob";
+    nameVariable = "Bob";
   }
 
   const xItem = randomValueFromArray(insertX);
