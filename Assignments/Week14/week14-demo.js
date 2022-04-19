3e32090f56c2f9d803fa8db6ba38cd09b2eb9ddf
jagreer7 <jagr4452@colorado.edu>
2022-04-19 10:00:29 -0600

    Created Week 14 JS

---
+++ Assignments/Week14/week14-demo.js
@@ -0,0 +1,16 @@
+const myCar = new Object();
+myCar.color = 'Gray';
+myCar.make = 'Mazda';
+myCar.model = 'CX-5';
+myCar.year = '2017';
+
+console.log(myCar.make);
+
+const mySecondCar = {
+  color: 'Blue',
+  make: 'Ford',
+  model: 'Ranger',
+  year: '2020'
+}
+
+console.log(mySecondCar.make);
