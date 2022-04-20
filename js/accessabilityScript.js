031c7180526224cece6870e1c830bbcb342807ae
jagreer7 <jagr4452@colorado.edu>
2022-04-20 16:30:32 -0600

    Created accessibility Buttons html

---
+++ js/accessabilityScript.js
@@ -0,0 +1,18 @@
+
+function MonoChrome(){
+  console.log("MonoChrome");
+}
+
+document.querySelector('.MonoChrome').addEventListener('click', MonoChrome);
+
+function HighContrastD(){
+  console.log("HighContrastD");
+}
+
+document.querySelector('.HighContrastD').addEventListener('click', HighContrastD);
+
+function HighContrastL(){
+  console.log("HighContrastL");
+}
+
+document.querySelector('.HighContrastL').addEventListener('click', HighContrastL);
