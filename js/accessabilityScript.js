ef3946b53362e52df2b19c538a3575f6bda956a9
jagreer7 <jagr4452@colorado.edu>
2022-04-20 17:19:31 -0600

    Monochrome js

--- js/accessabilityScript.js
+++ js/accessabilityScript.js
@@ -1,6 +1,28 @@
 
 function MonoChrome(){
   console.log("MonoChrome");
+  document.body.style.backgroundColor = "#fafafa";
+  document.querySelector('.headerDiv').style.backgroundColor = "#1e1e1e";
+  document.querySelector('.whiteBR').style.backgroundColor = "#1e1e1e";
+  document.querySelector('.bigTitle').style.backgroundColor = "#1e1e1e";
+  document.querySelector('h1').style.backgroundColor = "#1e1e1e";
+  document.querySelector('h2').style.color = "#1e1e1e";
+  document.querySelector('.accessabilityFlex').style.backgroundColor = "#1e1e1e";
+  document.querySelector('.underlineTitle').style.textShadow = "2px 2px #1e1e1e, 2px -2px #1e1e1e, -2px 2px #1e1e1e, -2px -2px #1e1e1e";
+  if(document.title === "Homepage Jackson Greer"){
+    document.querySelector("img").src="img/StairsBlue1_JG-MONO.JPG";
+  }
+  if(document.title === "About Me Jackson Greer"){
+    document.querySelector('.mediaPostButton').style.backgroundColor = "#1e1e1e";
+    document.querySelector("img").src="img/Rock_JG-MONO.JPG";
+  }
+  const flexes = document.querySelectorAll('.flex');
+  flexes.forEach(thing => {thing.style.backgroundColor = "#1e1e1e";});
+  const children = document.querySelectorAll('.child');
+  children.forEach(thing => {thing.style.backgroundColor = "#1e1e1e";});
+  const accessChild = document.querySelectorAll('.accessChild');
+  accessChild.forEach(thing => {thing.style.backgroundColor = "#1e1e1e";});
+
 }
 
 document.querySelector('.MonoChrome').addEventListener('click', MonoChrome);
@@ -16,3 +38,9 @@ function HighContrastL(){
 }
 
 document.querySelector('.HighContrastL').addEventListener('click', HighContrastL);
+
+function Classic(){
+  console.log("Classic");
+}
+
+document.querySelector('.Classic').addEventListener('click', Classic);
